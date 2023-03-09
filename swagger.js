58ec6e8ce9cc72fe7edee8a6f0e2f8e29bdd278d
Rony <rnicksonaxter@gmail.com>
2023-03-09 15:33:33 -0300

    putting to work

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My Sports API',
     description: 'Description',
   },
-  host: ['localhost:8080','cse-341-project2.onrender.com'],
+  host: ['cse-341-project2.onrender.com'],
   schemes: ['https', 'http'],
 };
 
